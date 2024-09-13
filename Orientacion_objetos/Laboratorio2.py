61e1acf8861665f8950bc4752b0ea3f524180e2a
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-09-13 05:04:44 +0000

    :zap: ITzKevinn was here!!

--- Orientacion_objetos/Laboratorio2.py
+++ Orientacion_objetos/Laboratorio2.py
@@ -1,16 +1,17 @@
 import random
  
 class Pieza:
-    def __init__(self, forma, color, posicion):
+    
+    def __init__(self, forma, color):
         self._forma = forma
         self._color = color
         self._posicion = 0
         self._estado = "creada"
-    
+        self._estados_validos = ["creada", "cayendo", "caida"]
+        self._colores_validos = ["rojo", "azul", "verde","amarillo"]
     
     def cambiar_estado(self, nuevo_estado):
-        estados_validos = ["creada", "cayendo", "caida"]
-        if nuevo_estado not in estados_validos:
+        if nuevo_estado not in self._estados_validos:
             raise ValueError("El estado debe ser creada, cayendo o caida")
         self._estado = nuevo_estado
 
@@ -20,11 +21,9 @@ class Pieza:
     
     @color.setter
     def color(self, color):
-        colores_validos = ["rojo", "azul", "verde","amarillo"]
-        if color not in colores_validos:
+        if color not in self.colores_validos:
             raise ValueError("El color debe ser rojo, azul, verde o amarillo")
         self._color = color
-      
     
     def mover(self, direccion):
         if direccion == "izquierda":
@@ -41,21 +40,20 @@ class Pieza:
     
 # clase juego 
 class juego:
+
     def __init__(self):
         self._piezas = []
         self._puntuacion = 0
         self._pieza_en_juego = []
     
-    
     def generar_pieza_aleatoria(self):
         #generar las formas y colores aleatorios
-        formas = ["I", "O", "T","L","J","S","Z"]
+        formas = ["I","O","T","L","J","S","Z"]
         colores = ["rojo", "azul", "verde","amarillo"]
         forma = random.choice(formas)
         color = random.choice(colores)
-        posicion = 0
-        nueva_pieza = Pieza(forma, posicion, color)
-        print (f"generada pieza:{nueva_pieza}")
+        nueva_pieza = Pieza(forma, color)
+        print (f"Agregar pieza: {nueva_pieza}")
         return nueva_pieza
     
     def agregar_pieza(self, pieza):
@@ -67,17 +65,32 @@ class juego:
         if len(self._piezas) == 0:
             print ("No hay más piezas para jugar...")
             return
+        self._piezas[0].cambiar_estado("cayendo")
         self._pieza_en_juego.append(self._piezas[0]) 
-        print (f"Jugando pieza: {self._piezas[0]}")
-        self._piezas.pop(0)
+        print (f"Jugando pieza: {self._piezas[0]}, la pieza esta cayendo |(°o°)|")
         self._puntuacion += 5
-        print (f"punticacion actual: {self._puntuacion}")
+        print (f"Punticacion actual: {self._puntuacion}")
+        self._piezas.pop(0)
     
       
     def mostar_pieza(self):
-        print ("piezas disponibles en el juego...")
+        if self._piezas.__len__() == 0:
+            print ("No hay piezas disponibles...")
+            return
+        
+        print ("Piezas disponibles en el juego, pero no en juego...")
         for pieza in self._piezas:
-            print (pieza)
+            print(f"--> {pieza}")
+            
+    def mostar_pieza_caida(self):
+        if self._pieza_en_juego.__len__() == 0:
+            print ("No hay piezas caidas...")
+            return
+        
+        print ("Piezas caidas del juego...")
+        for pieza in self._pieza_en_juego:
+            if pieza._estado == "caida":
+                print(f"--> {pieza}")
     
     def verificar_juego(self):
         if random.randint(1, len(self._piezas)+1) == 1:
@@ -85,24 +98,24 @@ class juego:
             return False
         return True
     
-    def pieza_cayo(self,pieza):
-        print(pieza)
-        if pieza.estado == "caida":
-            pieza.cambiar_estado("caida")
-            print (f"La Pieza: {pieza.forma} ha caído y su estado es {pieza._estado}...")
-            self.pieza_en_juego = None
+    def pieza_cayo(self, piezaActual):
+        if piezaActual._estado == "cayendo":
+            piezaActual.cambiar_estado("caida")
+            print (f"La Pieza: {piezaActual._forma} ha caído y su estado es {piezaActual._estado}...")
         else:
             print ("No hay piezas cayendo actualmente...")
     
     # MENÚ DEL JUEGO
 def menu():
-    print (""" ¿ que desea hacer?
+    print("\n")
+    print (""" ¿Que desea hacer?
             1. Generar pieza...
             2. Jugar pieza...
             3. Mover pieza (izquierda/drecha)
             4. Mostrar piezas...
-            5. caer pieza actual
-            6. Salir...
+            5. Caer pieza actual
+            6. Mostrar piezas caidas...
+            7. Salir...
             """)
     
     # funciones principale del juego
@@ -121,19 +134,29 @@ def main():
                 mi_juego.jugar_pieza()
 
         elif opcion == "3":
-            print ( mi_juego._piezas[0])
+            if mi_juego._pieza_en_juego.__len__() == 0:
+                print ("No hay piezas en el juego o ya se jugaron...")
+                continue
             direccion = input("¿A dónde mover? (izquierda/derecha)")
-            mi_juego._piezas[0].mover(direccion)
+            ultimaPieza = mi_juego._pieza_en_juego[mi_juego._pieza_en_juego.__len__() - 1]
+            ultimaPieza.mover(direccion)
             
         elif opcion == "4":
             mi_juego.mostar_pieza()
             
         elif opcion == "5":
-           mi_juego.pieza_cayo(mi_juego._pieza_en_juego[0])
+            if mi_juego._pieza_en_juego.__len__() == 0:
+                print ("No hay piezas para caer o no esta en juego...")
+                continue
+            mi_juego.pieza_cayo(mi_juego._pieza_en_juego[mi_juego._pieza_en_juego.__len__() - 1])
             
         elif opcion == "6":
+            mi_juego.mostar_pieza_caida()
+        
+        elif opcion == "7":
             print ("saliendo del juego...")
             break
+        
         else:
             print ("Opción invalida. Intente nuevamente...")
 
