47ddc68a9dc76df6907149e8c93ee2f9c7c98c0c
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-11-08 02:26:15 +0000

    mensajes qt:)print(f"Contraseña: {passwd}")

--- QT_y_python/main.py
+++ QT_y_python/main.py
@@ -4,13 +4,19 @@ from PyQt6.QtWidgets import QApplication
 from pathlib import Path
 ruta = Path(__file__).parent.resolve()
 
-def saludar():
-    print ("Saludar")
+def login():
+    global ventana
+    user = ventana.user.text()
+    passwd = ventana.passwd.text()
+    print(f"Usuario:{user}")
+    print(f"Contraseña: {passwd}")
+
     
 app = QApplication([])
 ventana = uic.loadUi(f"{ruta}/gui/principal.ui")
 
-ventana.btn_iniciar.clicked.connect(saludar)
+
+ventana.btn_iniciar.clicked.connect(login)
 
 ventana.show()
 app.exec()
